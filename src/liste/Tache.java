5ed05b7f129ed298a734af627ed872d98bd8206a
unknown <amaanali@outlook.fr>
2023-03-13 22:07:31 +0100

    J'ai créer la classe et les méthodes adéquates comme la méthode creerTache

--- src/liste/Tache.java
+++ src/liste/Tache.java
@@ -1,14 +1,20 @@
 package liste;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Tache {
     private String nom;
     private String description;
     private boolean est_realise;
     private int ref_type;
     private int ref_liste;
+    Connection maco = DriverManager.getConnection("jdbc:mysql://localhost:3306/todolist", "root", "");
 
 
-    public Tache(String nom, String description, boolean est_realise, int ref_type, int ref_liste){
+    public Tache(String nom, String description, boolean est_realise, int ref_type, int ref_liste) throws SQLException {
         this.nom = nom;
         this.description = description;
         this.est_realise = est_realise;
@@ -16,6 +22,14 @@ public class Tache {
         this.ref_liste = ref_liste;
     }
 
-
+    public void creerTache(String nom, String description, boolean est_realise,int ref_type, int ref_liste) throws SQLException {
+        PreparedStatement crea = maco.prepareStatement("INSERT INTO liste (nom, description, est_realise, ref_liste, ref_type) VALUES (?,?,?,?,?)");
+        crea.setString(1,nom);
+        crea.setString(2,description);
+        crea.setBoolean(3,est_realise);
+        crea.setInt(4,ref_liste);
+        crea.setInt(5,ref_type);
+        crea.executeUpdate();
+    }
 
 }
